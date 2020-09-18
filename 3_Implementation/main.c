7c3834d195993e07fe8af6c9754f64012ebafd46
99002685 <71494829+99002685@users.noreply.github.com>
2020-09-18 19:25:46 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,1 +1,36 @@
+#include "header.h"
+int main()
+{
+    char option;
+    char f2f[50] = "http://fresh2refresh.com/";
+    num_acc=0;
+    while(1)
+    {
+       printf("\n***** Welcome to Bank Application *****\n");
+       display_options();
+       printf("Please enter any options (1/2/3/4/5/6) ");
+       printf("to continue : ");
 
+        option = getch();
+        printf("%c \n", option);
+        switch(option)
+        {
+          case '1': Create_new_account();
+                    break;
+          case '2': Cash_Deposit();
+                    break;
+          case '3': Cash_withdrawl();
+                    break;
+          case '4': Account_information();
+                    break;
+          case '5': return 0;
+          case '6': system("cls");
+                    break;
+          default : system("cls");
+                    printf("Please enter one of the options");
+                    printf("(1/2/3/4/5/6) to continue \n ");
+                    break;
+        }
+    }
+    return 0;
+}
